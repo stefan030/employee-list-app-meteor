e961a945070d1da5ea4064618d9f4c60db357669
stelle030 <stefanbogdanovic030@gmail.com>
2019-02-07 16:23:44 +0100

    Employee details component creation and styling

--- client/components/employee_list.js
+++ client/components/employee_list.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
 const EmployeeList = (props) => {
     // props.employees => an array of employee objects
-
+    console.log(props.employees);
     return(
         <div>
-            <div className='employee-list'>
-                Employee List
+            <div className="employee-list">
+                {props.employees.map(employee => {
+                    return <EmployeeDetail key={employee._id} employee={employee} />
+                })}
             </div>
         </div>
     )
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,7 +6,6 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Employee list!</h1>
                 <EmployeeList />
             </div>
         )
